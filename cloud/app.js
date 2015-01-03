3387aff05e9e4b4b6649ee44827b06fc321801ea
sbppy <jie.cai2@gmail.com>
2015-01-04 00:31:47 +0800

    try get

--- cloud/app.js
+++ cloud/app.js
@@ -72,11 +72,12 @@ app.use(function(req, res, next) {
     var userid = urlPath.substr(2);
     var i = false;
     //query.equalTo("userid", userid);
-    query.find({
+    //query.find({
+    query.get("54a7e20be4b02eae43d254a7",{
       success: function(results) {
-        for (var i = 0; i < results.length; i++) {
-          var currentUser = results[i];
-          req.wechat_token = currentUser.get('token');
+        //for (var i = 0; i < results.length; i++) {
+        //  var currentUser = results[i];
+        //  req.wechat_token = currentUser.get('token');
           i = true;
         }
       },
