7173d945ac1ed3fd89f67890359a08a068d2af3b
sbppy <jie.cai2@gmail.com>
2015-01-03 14:41:43 +0800

    Update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -86,7 +86,7 @@ app.use(function(req, res, next) {
 //  res.end('hello node api');
 //}));
 
-app.use('/u1234', wechat( usertoken, function (req, res, next) {
+app.use('/u*', wechat( usertoken, function (req, res, next) {
   res.writeHead(200);
   res.end('hello node api');
 }));
