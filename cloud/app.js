1dd325fc7dc3b3c0e3025a079ce2b0aeeaa37e5b
sbppy <jie.cai2@gmail.com>
2015-01-03 11:57:14 +0800

    Update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -86,14 +86,14 @@ app.use(function(req, res, next) {
 //  res.end('hello node api');
 //}));
 
-app.use('/u1234', wechat( '', function (req, res, next) {
+app.use('/u123', wechat( '', function (req, res, next) {
   res.writeHead(200);
   res.end('hello node api');
 }));
 
-app.use('/u123', function (req, res, next) {
+app.use('/u1234', function (req, res, next) {
   res.writeHead(200);
-  res.end(user.get("token"));
+  res.end(req.wechat_token);
 });
 //app.get('/u*', wechat( config, function (req, res, next) {
 //  res.writeHead(200);
