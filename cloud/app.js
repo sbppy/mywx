3ffdfda1f03a0b19ef70017cb914272f13193afb
sbppy <jie.cai2@gmail.com>
2015-01-04 16:24:30 +0800

    parse to int

--- cloud/app.js
+++ cloud/app.js
@@ -69,7 +69,7 @@ app.use(function(req, res, next) {
   //var printtype = '';
   
   if (urlPath.substr(0,2) == '/u'){
-    query.equalTo("userid", userid);
+    query.equalTo("userid", parseInt(userid));
     query.first({
       success: function(currentUser) {
         //for (var i = 0; i < results.length; i++) {
