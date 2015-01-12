1a58429600956b6709b6d943be39ceb794f05e7b
sbppy <jie.cai2@gmail.com>
2015-01-12 18:28:58 +0800

    save

--- cloud/app.js
+++ cloud/app.js
@@ -49,14 +49,8 @@ var api = new WechatAPI(accessConfig.appid, accessConfig.secret, function (callb
   query.first({
     success: function(currentUser) {
       if (currentUser){
-        currentUser.save(null, {
-          success: function(currentUser) {
-            // Now let's update it with some new data. In this case, only cheatMode and score
-            // will get sent to the cloud. playerName hasn't changed.
-            currentUser.set('accessToken', JSON.stringify(token));
-            currentUser.save();
-          }
-        });
+        currentUser.set('accessToken', JSON.stringify(token));
+        currentUser.save();
       }
     },
     error: function(error) {
